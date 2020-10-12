b62a49213f647ec7c04b63fca6a8474c7f39764a
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2020-10-12 17:53:55 -0300

    incluindo os arquivos

---
+++ modulo-01/js/fuction.js
@@ -0,0 +1,29 @@
+const Sala1 = [
+  {
+    nome: 'Johnny',
+    idade: 26,
+  },
+  {
+    nome: 'Tatiana',
+    idade: 25,
+  },
+
+  {
+    nome: 'Carlos',
+    idade: 32,
+  },
+
+  {
+    nome: 'Roberto',
+    idade: 62,
+  },
+];
+
+function totalDeAlunos(Sala1) {
+  if (nome === idade) {
+    console.log(`${nome}, voce tem odode`);
+    return;
+  }
+}
+
+
